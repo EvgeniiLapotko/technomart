9090ff975ddf86f9c5e517a7b11e74f0430547e7
Evgenii <lapotko.e@yandex.ru>
2020-12-11 00:30:05 +0300

    added header and card and slick slider

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function () {
+
+
+
+    $('.slick-slid').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        arrows: false,
+    });
+
+
+});
